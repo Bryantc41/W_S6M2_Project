15b547b57a9c972d44720538852ac99b1d524c43
Bryant Clark <bryantc4110@yahoo.com>
2024-08-29 17:08:38 -0400

    done

--- frontend/components/Posts/Posts.js
+++ frontend/components/Posts/Posts.js
@@ -9,6 +9,7 @@ const Posts = (props) => {
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+  {posts.map(pt => <Post likePost={likePost} post={pt} key={pt.id} />) }
     </div>
   );
 };
